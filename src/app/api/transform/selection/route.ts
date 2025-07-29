02daf5fae1fd42d06104a408a669c49f9f81523f
Yaroslav Klimenko <klimyarik13@gmail.com>
2025-07-29 22:18:02 +0300

    smth

--- src/app/api/transform/selection/route.ts
+++ src/app/api/transform/selection/route.ts
@@ -99,7 +99,7 @@ export async function POST(request: Request) {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "strict",
-        maxAge: 60 * 60 * 24 * 7, // 7 days
+        maxAge: 60 * 60 * 24 * 7, // 7 days_
       });
     }
 
